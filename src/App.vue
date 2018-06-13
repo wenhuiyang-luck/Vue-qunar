d4db4cbbea3fc2784af1c659f90b9f136e949e72
wenhui yang <ywhluck@gmail.com>
2018-06-13 20:36:58 +0800

    finish page

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <router-view/>
+    <keep-alive>
+      <router-view/>
+    </keep-alive>
   </div>
 </template>
 
@@ -11,4 +13,5 @@ export default {
 </script>
 
 <style>
+
 </style>
